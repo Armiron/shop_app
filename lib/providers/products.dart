93940bb20a77e9da6fdfe41c4d3748cde74243d6
Armiron Kurbalaj <armironkurbalaj@gmail.com>
2024-05-01 20:25:02 +0200

    Added way to add new product

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -65,8 +65,24 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  void addProduct() {
-    // _items.add(value);
+  void addProduct(Product product) {
+    final newProduct = Product(
+      id: DateTime.now().toString(),
+      title: product.title,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+    );
+    _items.add(newProduct);
     notifyListeners();
+  }
+
+  void updateProduct(String id, Product newProduct) {
+    final productIndex = _items.indexWhere((element) => element.id == id);
+
+    if (productIndex >= 0) {
+      _items[productIndex] = newProduct;
+      notifyListeners();
+    }
   }
 }
--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
+import '../providers/product.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product';
   const EditProductScreen({super.key});
 
   @override
-  State<EditProductScreen> createState() => _EditProductScreenState();
+  State<EditProductScreen> createState() {
+    return _EditProductScreenState();
+  }
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
@@ -13,11 +18,51 @@ class _EditProductScreenState extends State<EditProductScreen> {
   final _descriptionFocusNode = FocusNode();
   final _imageUrlController = TextEditingController();
   final _imageUrlFocusNode = FocusNode();
+  final _form = GlobalKey<FormState>();
+  var _editedProduct = Product(
+    id: '',
+    title: '',
+    description: '',
+    price: 0,
+    imageUrl: '',
+    isFavorite: false,
+  );
+
+  var _initValues = {
+    'title': '',
+    'description': '',
+    'price': '',
+    'imageUrl': '',
+  };
 
   @override
   void initState() {
     _imageUrlFocusNode.addListener(_updateImageUrl);
     super.initState();
+  }
+
+  var _isInit = true;
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      final productId = ModalRoute.of(context)?.settings.arguments;
+      if (productId != null && productId is String && productId != '') {
+        _editedProduct =
+            Provider.of<Products>(context, listen: false).findById(productId);
+
+        _initValues = {
+          'title': _editedProduct.title,
+          'description': _editedProduct.description,
+          'price': _editedProduct.price.toString(),
+          // 'imageUrl': _editedProduct.imageUrl,
+          // 'imageUrl': '',
+        };
+        _imageUrlController.text = _editedProduct.imageUrl;
+        _isInit = false;
+      }
+    }
+    super.didChangeDependencies();
   }
 
   @override
@@ -32,28 +77,73 @@ class _EditProductScreenState extends State<EditProductScreen> {
 
   void _updateImageUrl() {
     if (!_imageUrlFocusNode.hasFocus) {
+      var value = _imageUrlController.text;
+      if (value.isEmpty &&
+          !value.startsWith('http') &&
+          !value.startsWith('https')) {
+        return;
+      }
       setState(() {});
     }
+  }
+
+  void _saveForm() {
+    final isValid = _form.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+    _form.currentState?.save();
+    if (_editedProduct.id != '') {
+      Provider.of<Products>(context, listen: false)
+          .updateProduct(_editedProduct.id, _editedProduct);
+    } else {
+      Provider.of<Products>(context, listen: false).addProduct(_editedProduct);
+    }
+    Navigator.of(context).pop();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Edit Product')),
+        appBar: AppBar(
+          title: const Text('Edit Product'),
+          actions: <Widget>[
+            IconButton(onPressed: _saveForm, icon: const Icon(Icons.save))
+          ],
+        ),
         body: Padding(
           padding: const EdgeInsets.all(16),
           child: Form(
+            key: _form,
             child: ListView(
               children: <Widget>[
                 TextFormField(
+                  initialValue: _initValues['title'],
                   decoration: const InputDecoration(labelText: 'Title'),
                   textInputAction: TextInputAction.next,
                   onFieldSubmitted: (value) {
                     // It did it automatically for me (might be new version)
                     FocusScope.of(context).requestFocus(_priceFocusNode);
                   },
+                  onSaved: (newValue) {
+                    _editedProduct = Product(
+                      id: _editedProduct.id,
+                      title: newValue!,
+                      description: _editedProduct.description,
+                      price: _editedProduct.price,
+                      imageUrl: _editedProduct.imageUrl,
+                      isFavorite: _editedProduct.isFavorite,
+                    );
+                  },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Provide a value';
+                    }
+                    return null;
+                  },
                 ),
                 TextFormField(
+                  initialValue: _initValues['price'],
                   decoration: const InputDecoration(labelText: 'Price'),
                   textInputAction: TextInputAction.next,
                   keyboardType: TextInputType.number,
@@ -62,12 +152,54 @@ class _EditProductScreenState extends State<EditProductScreen> {
                     // It did it automatically for me (might be new version)
                     FocusScope.of(context).requestFocus(_descriptionFocusNode);
                   },
+                  onSaved: (newValue) {
+                    _editedProduct = Product(
+                      id: _editedProduct.id,
+                      title: _editedProduct.title,
+                      description: _editedProduct.description,
+                      price: double.parse(newValue!),
+                      imageUrl: _editedProduct.imageUrl,
+                      isFavorite: _editedProduct.isFavorite,
+                    );
+                  },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Provide a value';
+                    }
+                    if (double.tryParse(value) == null) {
+                      return 'Enter a valid number';
+                    }
+                    if (double.parse(value) <= 0) {
+                      return 'Please enter a number > 0';
+                    }
+                    return null;
+                  },
                 ),
                 TextFormField(
+                  initialValue: _initValues['description'],
                   decoration: const InputDecoration(labelText: 'Description'),
                   maxLines: 3,
                   keyboardType: TextInputType.multiline,
                   focusNode: _descriptionFocusNode,
+                  onSaved: (newValue) {
+                    _editedProduct = Product(
+                      id: _editedProduct.id,
+                      title: _editedProduct.title,
+                      description: newValue!,
+                      price: _editedProduct.price,
+                      imageUrl: _editedProduct.imageUrl,
+                      isFavorite: _editedProduct.isFavorite,
+                    );
+                  },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Provide a description';
+                    }
+                    if (value!.length < 10) {
+                      return 'Should be at least 10 char long';
+                    }
+                    return null;
+                  },
                 ),
                 Row(
                   crossAxisAlignment: CrossAxisAlignment.end,
@@ -89,12 +221,39 @@ class _EditProductScreenState extends State<EditProductScreen> {
                     ),
                     Expanded(
                       child: TextFormField(
+                        initialValue: _initValues['imageUrl'],
                         decoration:
                             const InputDecoration(labelText: 'Image URL'),
                         keyboardType: TextInputType.url,
                         textInputAction: TextInputAction.done,
-                        controller: _imageUrlController,
+                        controller: _isInit ? _imageUrlController : null,
                         focusNode: _imageUrlFocusNode,
+                        onFieldSubmitted: (value) => {_saveForm()},
+                        onSaved: (newValue) {
+                          _editedProduct = Product(
+                            id: _editedProduct.id,
+                            title: _editedProduct.title,
+                            description: _editedProduct.description,
+                            price: _editedProduct.price,
+                            imageUrl: newValue!,
+                            isFavorite: _editedProduct.isFavorite,
+                          );
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Provide an image url';
+                          }
+                          if (!value.startsWith('http') &&
+                              !value.startsWith('https')) {
+                            return 'Please enter a valid url';
+                          }
+                          // if (!value.endsWith('.png') &&
+                          //     !value.endsWith('.jpg') &&
+                          //     !value.endsWith('.jpeg')) {
+                          //   return 'Please enter a valid url';
+                          // }
+                          return null;
+                        },
                       ),
                     )
                   ],
