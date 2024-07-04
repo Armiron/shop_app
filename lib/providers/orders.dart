4442b02a94697561e230259c69b006077589c2eb
Armiron Kurbalaj <armironkurbalaj@gmail.com>
2024-07-04 18:28:46 +0200

    Added orders to db, also implemented fetch on orders page

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 import 'cart.dart';
 
 class OrderItem {
@@ -22,16 +26,69 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
-    _orders.insert(
-      0,
-      OrderItem(
-        id: DateTime.now().toString(),
-        amount: total,
-        products: cartProducts,
-        dateTime: DateTime.now(),
-      ),
-    ); // To the beginning
-    notifyListeners();
+  Future<void> fetchAndSetOrders() async {
+    const urlString =
+        "https://testflutterproject-719b6-default-rtdb.europe-west1.firebasedatabase.app/orders.json";
+    Uri url = Uri.parse(urlString);
+    try {
+      final response = await http.get(url);
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      final List<OrderItem> loadedOrders = [];
+      extractedData.forEach((orderId, orderData) {
+        loadedOrders.add(OrderItem(
+          id: orderId,
+          amount: orderData['amount'],
+          dateTime: DateFormat('yMMdd:HH:mm.sss').parse(orderData['dateTime']),
+          products: (orderData['products'] as List<dynamic>)
+              .map((product) => CartItem(
+                    id: product['id'],
+                    title: product['title'],
+                    quantity: product['quantity'],
+                    price: product['price'],
+                  ))
+              .toList(),
+        ));
+      });
+      _orders = loadedOrders;
+      print('done');
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    const urlString =
+        "https://testflutterproject-719b6-default-rtdb.europe-west1.firebasedatabase.app/orders.json";
+    Uri url = Uri.parse(urlString);
+    // for (var cart in cartProducts) {
+    //   print(cart);
+    // }
+    try {
+      final response = await http.post(url,
+          body: jsonEncode({
+            "amount": total,
+            "products": jsonEncode(cartProducts
+                .map((cartItem) => {
+                      'id': cartItem.id,
+                      'title': cartItem.title,
+                      'quantity': cartItem.quantity,
+                      'price': cartItem.price,
+                    })
+                .toList()),
+            "dateTime": DateFormat('yMMdd:HH:mm.sss').format(DateTime.now())
+          }));
+      _orders.insert(
+        0,
+        OrderItem(
+          id: json.decode(response.body)['name'],
+          amount: total,
+          products: cartProducts,
+          dateTime: DateTime.now(),
+        ),
+      ); // To the beginning
+      notifyListeners();
+    } catch (e) {
+      rethrow;
+    }
   }
 }
--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,9 +4,30 @@ import '../widgets/app_drawer.dart';
 import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = '/order';
   const OrdersScreen({super.key});
+
+  @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  var isLoading = false;
+
+  @override
+  void initState() {
+    Future.delayed(Duration.zero).then((value) async {
+      setState(() {
+        isLoading = true;
+      });
+      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+      setState(() {
+        isLoading = false;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +36,15 @@ class OrdersScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Your Orders'),
       ),
-      body: ListView.builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: (context, index) => OrderItem(orderData.orders[index]),
-      ),
+      body: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: orderData.orders.length,
+              itemBuilder: (context, index) =>
+                  OrderItem(orderData.orders[index]),
+            ),
       drawer: AppDrawer(),
     );
   }
